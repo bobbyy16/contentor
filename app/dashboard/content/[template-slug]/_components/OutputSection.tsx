3077207851d51d0603d8169e671d473729dfb246
Abhishek k <bobbyyyyy.16@gmail.com>
2024-11-03 13:16:10 +0530

    Feature: Rich text editor

--- app/dashboard/content/[template-slug]/_components/OutputSection.tsx
+++ app/dashboard/content/[template-slug]/_components/OutputSection.tsx
@@ -1,7 +1,33 @@
-import React from "react";
+import React, { useRef } from "react";
+import "@toast-ui/editor/dist/toastui-editor.css";
+
+import { Editor } from "@toast-ui/react-editor";
+import { Copy } from "lucide-react";
+import { Button } from "@/components/ui/button";
 
 const OutputSection = () => {
-  return <div>OutputSection</div>;
+  const editorRef: any = useRef();
+  return (
+    <div className="bg-white shadow-lg border rounded-lg">
+      <div className="flex justify-between items-center p-5">
+        <h2>Your Result</h2>
+        <Button>
+          <Copy />
+          Copy
+        </Button>
+      </div>
+      <Editor
+        ref={editorRef}
+        initialValue="Your result will appear here"
+        initialEditType="wysiwyg"
+        height="600px"
+        useCommandShortcut={true}
+        onChange={() =>
+          console.log(editorRef.current.getInstance().getMarkdown())
+        }
+      />
+    </div>
+  );
 };
 
 export default OutputSection;
