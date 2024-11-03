b8155d209825075544bbdb491a3159998d1e9019
Abhishek k <bobbyyyyy.16@gmail.com>
2024-11-03 10:16:30 +0530

    delete: unwanted Templates

--- app/(data)/Templates.tsx
+++ app/(data)/Templates.tsx
@@ -1,26 +1,26 @@
 export default [
-  {
-    name: "Blog Title",
-    desc: "An AI tool that generate blog title depends on yout blog information",
-    category: "Blog",
-    icon: "https://cdn-icons-png.flaticon.com/128/2799/2799954.png",
-    aiPrompt:
-      "Give me 5 blog topic idea in bullet wise only based on give niche & outline and give me result in Rich text editor format",
-    slug: "generate-blog-title",
-    form: [
-      {
-        label: "Enter your blog niche",
-        field: "input",
-        name: "niche",
-        required: true,
-      },
-      {
-        label: "Enter blog outline",
-        field: "textarea",
-        name: "outline",
-      },
-    ],
-  },
+  // {
+  //   name: "Blog Title",
+  //   desc: "An AI tool that generate blog title depends on yout blog information",
+  //   category: "Blog",
+  //   icon: "https://cdn-icons-png.flaticon.com/128/2799/2799954.png",
+  //   aiPrompt:
+  //     "Give me 5 blog topic idea in bullet wise only based on give niche & outline and give me result in Rich text editor format",
+  //   slug: "generate-blog-title",
+  //   form: [
+  //     {
+  //       label: "Enter your blog niche",
+  //       field: "input",
+  //       name: "niche",
+  //       required: true,
+  //     },
+  //     {
+  //       label: "Enter blog outline",
+  //       field: "textarea",
+  //       name: "outline",
+  //     },
+  //   ],
+  // },
   {
     name: "Blog Content",
     desc: "An AI tool that serves as your personal blog post title writer, generating catchy and viral-worthy titles in your chosen language.",
@@ -43,110 +43,110 @@ export default [
       },
     ],
   },
-  {
-    name: "Blog Topic Ideas",
-    desc: "An AI tool that serves as your personal blog post title writer, generating catchy and viral-worthy titles in your chosen language.",
-    category: "Blog",
-    icon: "https://cdn-icons-png.flaticon.com/128/16104/16104107.png",
-    slug: "blog-topic-idea",
-    aiPrompt:
-      "Generate top 5 Blog Topic Ideas in bullet point only, (no Description) based on niche in rich text editor format",
-    form: [
-      {
-        label: "Enter your Niche",
-        field: "input",
-        name: "niche",
-        required: true,
-      },
-    ],
-  },
-  {
-    name: "Youtube SEO Title",
-    desc: "An AI tool that serves as your personal blog post title writer, generating catchy and viral-worthy titles in your chosen language.",
-    category: "Youtube Tools",
-    icon: "https://cdn-icons-png.flaticon.com/128/11461/11461589.png",
-    slug: "youtube-seo-title",
-    aiPrompt:
-      "Give me Best SEO optimized high ranked 5 title ideas bullet wise only bases on keywords and outline and give me result in HTML tags format",
-    form: [
-      {
-        label: "Enter your youtube video topic keyowords",
-        field: "input",
-        name: "keywords",
-        required: true,
-      },
-      {
-        label: "Enter youtube description Outline here",
-        field: "textarea",
-        name: "outline",
-      },
-    ],
-  },
-  {
-    name: "Youtube Description",
-    desc: "An AI tool that serves as your personal blog post title writer, generating catchy and viral-worthy titles in your chosen language.",
-    category: "Youtube Tool",
-    icon: "https://cdn-icons-png.flaticon.com/128/1384/1384028.png",
-    slug: "youtube-description",
-    aiPrompt:
-      "Generate Youtube description with emoji under 4-5 lines based on topic and outline in rich text editor format",
-    form: [
-      {
-        label: "Enter your blog topic/title",
-        field: "input",
-        name: "topic",
-        required: true,
-      },
-      {
-        label: "Enter youtube Outline here",
-        field: "textarea",
-        name: "outline",
-      },
-    ],
-  },
-  {
-    name: "Youtube Tags",
-    desc: "An AI tool that serves as your personal blog post title writer, generating catchy and viral-worthy titles in your chosen language.",
-    category: "Youtube Tool",
-    icon: "https://cdn-icons-png.flaticon.com/128/15841/15841074.png",
-    slug: "youtube-tag",
+  // {
+  //   name: "Blog Topic Ideas",
+  //   desc: "An AI tool that serves as your personal blog post title writer, generating catchy and viral-worthy titles in your chosen language.",
+  //   category: "Blog",
+  //   icon: "https://cdn-icons-png.flaticon.com/128/16104/16104107.png",
+  //   slug: "blog-topic-idea",
+  //   aiPrompt:
+  //     "Generate top 5 Blog Topic Ideas in bullet point only, (no Description) based on niche in rich text editor format",
+  //   form: [
+  //     {
+  //       label: "Enter your Niche",
+  //       field: "input",
+  //       name: "niche",
+  //       required: true,
+  //     },
+  //   ],
+  // },
+  // {
+  //   name: "Youtube SEO Title",
+  //   desc: "An AI tool that serves as your personal blog post title writer, generating catchy and viral-worthy titles in your chosen language.",
+  //   category: "Youtube Tools",
+  //   icon: "https://cdn-icons-png.flaticon.com/128/11461/11461589.png",
+  //   slug: "youtube-seo-title",
+  //   aiPrompt:
+  //     "Give me Best SEO optimized high ranked 5 title ideas bullet wise only bases on keywords and outline and give me result in HTML tags format",
+  //   form: [
+  //     {
+  //       label: "Enter your youtube video topic keyowords",
+  //       field: "input",
+  //       name: "keywords",
+  //       required: true,
+  //     },
+  //     {
+  //       label: "Enter youtube description Outline here",
+  //       field: "textarea",
+  //       name: "outline",
+  //     },
+  //   ],
+  // },
+  // {
+  //   name: "Youtube Description",
+  //   desc: "An AI tool that serves as your personal blog post title writer, generating catchy and viral-worthy titles in your chosen language.",
+  //   category: "Youtube Tool",
+  //   icon: "https://cdn-icons-png.flaticon.com/128/1384/1384028.png",
+  //   slug: "youtube-description",
+  //   aiPrompt:
+  //     "Generate Youtube description with emoji under 4-5 lines based on topic and outline in rich text editor format",
+  //   form: [
+  //     {
+  //       label: "Enter your blog topic/title",
+  //       field: "input",
+  //       name: "topic",
+  //       required: true,
+  //     },
+  //     {
+  //       label: "Enter youtube Outline here",
+  //       field: "textarea",
+  //       name: "outline",
+  //     },
+  //   ],
+  // },
+  // {
+  //   name: "Youtube Tags",
+  //   desc: "An AI tool that serves as your personal blog post title writer, generating catchy and viral-worthy titles in your chosen language.",
+  //   category: "Youtube Tool",
+  //   icon: "https://cdn-icons-png.flaticon.com/128/15841/15841074.png",
+  //   slug: "youtube-tag",
 
-    aiPrompt:
-      "Generate 10 Youtube tags in bullet point based on title and outline in rich text editor format",
+  //   aiPrompt:
+  //     "Generate 10 Youtube tags in bullet point based on title and outline in rich text editor format",
 
-    form: [
-      {
-        label: "Enter your youtube title",
-        field: "input",
-        name: "title",
-        required: true,
-      },
-      {
-        label: "Enter youtube video Outline here (Optional)",
-        field: "textarea",
-        name: "outline",
-      },
-    ],
-  },
+  //   form: [
+  //     {
+  //       label: "Enter your youtube title",
+  //       field: "input",
+  //       name: "title",
+  //       required: true,
+  //     },
+  //     {
+  //       label: "Enter youtube video Outline here (Optional)",
+  //       field: "textarea",
+  //       name: "outline",
+  //     },
+  //   ],
+  // },
 
-  {
-    name: "Rewrite Article (Plagiarism Free)",
-    desc: "Use this tool to rewrite existing Article or Blog Post which can bypass AI detectors and also make it plagiarism free.",
-    icon: "https://cdn-icons-png.flaticon.com/128/81/81460.png",
-    category: "Rewriting Tool",
-    slug: "rewrite-article",
-    aiPrompt:
-      "Rewrite give article without any Plagiarism in rich text editor format",
-    form: [
-      {
-        label:
-          "🤖 Provide your Article/Blogpost or any other content to rewrite.",
-        field: "textarea",
-        name: "article",
-        required: true,
-      },
-    ],
-  },
+  // {
+  //   name: "Rewrite Article (Plagiarism Free)",
+  //   desc: "Use this tool to rewrite existing Article or Blog Post which can bypass AI detectors and also make it plagiarism free.",
+  //   icon: "https://cdn-icons-png.flaticon.com/128/81/81460.png",
+  //   category: "Rewriting Tool",
+  //   slug: "rewrite-article",
+  //   aiPrompt:
+  //     "Rewrite give article without any Plagiarism in rich text editor format",
+  //   form: [
+  //     {
+  //       label:
+  //         "🤖 Provide your Article/Blogpost or any other content to rewrite.",
+  //       field: "textarea",
+  //       name: "article",
+  //       required: true,
+  //     },
+  //   ],
+  // },
   {
     name: "Text Improver",
     desc: "This handy tool refines your writing, eliminating errors and redundancies for a clear, readable result. It also offers a comprehensive tone analysis and suggests better word choices.",
@@ -180,26 +180,26 @@ export default [
       },
     ],
   },
-  {
-    name: "Instagram Post Generator",
-    desc: "An AI tool that serves as your personal blog post title writer, generating catchy and viral-worthy titles in your chosen language.",
-    icon: "https://cdn-icons-png.flaticon.com/128/1384/1384031.png",
-    category: "blog",
+  // {
+  //   name: "Instagram Post Generator",
+  //   desc: "An AI tool that serves as your personal blog post title writer, generating catchy and viral-worthy titles in your chosen language.",
+  //   icon: "https://cdn-icons-png.flaticon.com/128/1384/1384031.png",
+  //   category: "blog",
 
-    slug: "instagram-post-generator",
-    aiPrompt:
-      "Generate 3 Instagram post depends on a given keywords and give output in  in rich text editor format",
-    form: [
-      {
-        label: "Enter Keywords for your post",
-        field: "input",
-        name: "keywords",
-        required: true,
-      },
-    ],
-  },
+  //   slug: "instagram-post-generator",
+  //   aiPrompt:
+  //     "Generate 3 Instagram post depends on a given keywords and give output in  in rich text editor format",
+  //   form: [
+  //     {
+  //       label: "Enter Keywords for your post",
+  //       field: "input",
+  //       name: "keywords",
+  //       required: true,
+  //     },
+  //   ],
+  // },
   {
-    name: "Instagram Hash Tag Generator",
+    name: "Hash Tag Generator",
     desc: "An AI tool that serves as your personal blog post title writer, generating catchy and viral-worthy titles in your chosen language.",
     icon: "https://cdn-icons-png.flaticon.com/128/74/74559.png",
     category: "blog",
@@ -216,26 +216,26 @@ export default [
       },
     ],
   },
-  {
-    name: "Instagram Post/Reel Idea",
-    desc: "An AI tool that generate New and trending instagram idea depends on your niche",
-    icon: "https://cdn-icons-png.flaticon.com/128/14/14143.png",
-    category: "instagram",
+  // {
+  //   name: "Instagram Post/Reel Idea",
+  //   desc: "An AI tool that generate New and trending instagram idea depends on your niche",
+  //   icon: "https://cdn-icons-png.flaticon.com/128/14/14143.png",
+  //   category: "instagram",
 
-    slug: "instagram-post-idea-generator",
-    aiPrompt:
-      "Generate 5-10 Instagram idea depends on niche with latest trend and give output in  in rich text editor format",
-    form: [
-      {
-        label: "Enter Keywords / Niche for your instagram idea",
-        field: "input",
-        name: "keywords",
-        required: true,
-      },
-    ],
-  },
+  //   slug: "instagram-post-idea-generator",
+  //   aiPrompt:
+  //     "Generate 5-10 Instagram idea depends on niche with latest trend and give output in  in rich text editor format",
+  //   form: [
+  //     {
+  //       label: "Enter Keywords / Niche for your instagram idea",
+  //       field: "input",
+  //       name: "keywords",
+  //       required: true,
+  //     },
+  //   ],
+  // },
   {
-    name: "English Grammer Check",
+    name: "Grammer Check",
     desc: "AI Model to Correct your english grammer by providing the text",
     icon: "https://cdn-icons-png.flaticon.com/128/12596/12596743.png",
     category: "english",
@@ -306,52 +306,52 @@ export default [
       },
     ],
   },
-  {
-    name: "Tagline Generator",
-    desc: "Struggling to find the perfect tagline for your brand? Let our AI-tool assist you in creating a tagline that stands out.",
-    icon: "https://cdn-icons-png.flaticon.com/128/4213/4213148.png",
-    category: "Marketting",
+  // {
+  //   name: "Tagline Generator",
+  //   desc: "Struggling to find the perfect tagline for your brand? Let our AI-tool assist you in creating a tagline that stands out.",
+  //   icon: "https://cdn-icons-png.flaticon.com/128/4213/4213148.png",
+  //   category: "Marketting",
 
-    slug: "tagline-generator",
-    aiPrompt:
-      "Depends on user productName and outline generate catchy 5-10 tagline for the business product and give output  in rich text editor format ",
-    form: [
-      {
-        label: "Product/Brand Name",
-        field: "input",
-        name: "productName",
-        required: true,
-      },
-      {
-        label: "What you are selling / Marketting",
-        field: "textarea",
-        name: "outline",
-        required: true,
-      },
-    ],
-  },
-  {
-    name: "Product Description",
-    desc: "This is your AI-powered SEO expert, creating captivating and keyword-rich e-commerce product descriptions to boost your online sales.",
-    icon: "https://cdn-icons-png.flaticon.com/128/3528/3528145.png",
-    category: "Marketting",
+  //   slug: "tagline-generator",
+  //   aiPrompt:
+  //     "Depends on user productName and outline generate catchy 5-10 tagline for the business product and give output  in rich text editor format ",
+  //   form: [
+  //     {
+  //       label: "Product/Brand Name",
+  //       field: "input",
+  //       name: "productName",
+  //       required: true,
+  //     },
+  //     {
+  //       label: "What you are selling / Marketting",
+  //       field: "textarea",
+  //       name: "outline",
+  //       required: true,
+  //     },
+  //   ],
+  // },
+  // {
+  //   name: "Product Description",
+  //   desc: "This is your AI-powered SEO expert, creating captivating and keyword-rich e-commerce product descriptions to boost your online sales.",
+  //   icon: "https://cdn-icons-png.flaticon.com/128/3528/3528145.png",
+  //   category: "Marketting",
 
-    slug: "product-description",
-    aiPrompt:
-      "Depends on user productName and description generate small description for product for e-commer business give output  in rich text editor format  ",
-    form: [
-      {
-        label: "Product Name",
-        field: "input",
-        name: "productName",
-        required: true,
-      },
-      {
-        label: "Product Details",
-        field: "textarea",
-        name: "outline",
-        required: true,
-      },
-    ],
-  },
+  //   slug: "product-description",
+  //   aiPrompt:
+  //     "Depends on user productName and description generate small description for product for e-commer business give output  in rich text editor format  ",
+  //   form: [
+  //     {
+  //       label: "Product Name",
+  //       field: "input",
+  //       name: "productName",
+  //       required: true,
+  //     },
+  //     {
+  //       label: "Product Details",
+  //       field: "textarea",
+  //       name: "outline",
+  //       required: true,
+  //     },
+  //   ],
+  // },
 ];
