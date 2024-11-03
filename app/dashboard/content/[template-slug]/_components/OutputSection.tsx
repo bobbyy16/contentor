3879eacf0a44c4a42252995ef977cd833930753f
Abhishek k <bobbyyyyy.16@gmail.com>
2024-11-03 13:18:21 +0530

    enchancement: Imrpoved font size

--- app/dashboard/content/[template-slug]/_components/OutputSection.tsx
+++ app/dashboard/content/[template-slug]/_components/OutputSection.tsx
@@ -10,8 +10,8 @@ const OutputSection = () => {
   return (
     <div className="bg-white shadow-lg border rounded-lg">
       <div className="flex justify-between items-center p-5">
-        <h2>Your Result</h2>
-        <Button>
+        <h2 className="font-medium text-lg">Your Result</h2>
+        <Button className="flex gap-2">
           <Copy />
           Copy
         </Button>
